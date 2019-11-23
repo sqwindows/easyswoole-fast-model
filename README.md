1fa0a45944f663ba288730d3cada920a3a35a81d
sqwindows <sqwindows@qq.com>
2019-11-23 16:44:21 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## 简介
+  
+## 安装
+```bash
+composer require crazycater/easyswoole-fast-model
+```
+## 使用方法
+```
+ 
+
+```
---
+++ src/Fast.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by crazyCater
+ * User: crazyCater
+ * Date: 2019/11/20 15:10
+ */
+
+namespace EasySwooleFastModel;
+
+class Fast
+{
+
+}
